7b2c0202341881e64c6221e21629cba179d3ca54
“chonticha” <chonticha22745@gmail.com>
2025-01-21 17:42:58 +0700

    first commit

---
+++ src/app/(routeapp)/blogpost2/page.tsx
@@ -0,0 +1,10 @@
+import Blogpost2 from '@/component/Blogpost2'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      <Blogpost2/>
+    </div>
+  )
+}
---
+++ src/app/(routeapp)/service/page.tsx
@@ -0,0 +1,10 @@
+import Service from "@/component/Service";
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      <Service />
+    </div>
+  );
+}
