97efe5ee80abd6a9479106fb727a28a7ae6b0b31
“chutikan” <chutikanKK88@gmail.com>
2025-03-05 13:27:52 +0700

    first commit

--- src/component/Portfolio2.tsx
+++ src/component/Portfolio2.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 
 export default function Portfolio2() {
   return (
@@ -9,8 +9,7 @@ export default function Portfolio2() {
           <div className="container">
             {/* Page Heading/Breadcrumbs */}
             <h1 className="mt-4 mb-3">
-              การจัดกิจกรรมเพื่อการพัฒนาองค์การภาครัฐ
-              {/* <small>Subheading</small> */}
+              การจัดกิจกรรมเพื่อการพัฒนาองค์การภาครัฐ<small>Subheading</small>
             </h1>
           </div>
         </div>
@@ -21,28 +20,186 @@ export default function Portfolio2() {
               <li className="breadcrumb-item">
                 <a href="/">หน้าหลัก</a>
               </li>
-              <li className="breadcrumb-item active">การจัดกิจกรรมเพื่อการพัฒนาองค์การภาครัฐ
-</li>
+              <li className="breadcrumb-item active">
+                การจัดกิจกรรมเพื่อการพัฒนาองค์การภาครัฐ
+              </li>
             </ol>
           </div>
           <div className="row">
             <div className="col-lg-6 portfolio-item">
               <div className="card h-100">
                 <a href="#">
-                  <img
-                    className="card-img-top"
-                    src="images/portfolio-big-01.jpg"
-                    alt=""
-                  />
+                  <img className="card-img-top" src="images/p4.jpg" alt="" />
                 </a>
                 <div className="card-body">
                   <h4 className="card-title">
-                    <a href="#">Morbi eu nulla ac mauris euismod aliquet.</a>
+                    <a
+                      href="assets/Organization Intercvenhtion/Organization Intercvenhtion 1-41.pdf"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      การบริหารผลงาน{" "}
+                    </a>
                   </h4>
                   <p className="card-text">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
-                    viverra euismod odio, gravida pellentesque urna varius
-                    vitae.
+                    การบริหารผลงาน คือ กระบวนการบริหารเชิงยุทธศาสตร์
+                    ที่มุ่งทำให้ทั้งองค์การและบุคลากร
+                    ทำงานบรรลุผลสำเร็จตามเป้าหมาย
+                    โดยการปรับปรุงผลการปฏิบัติงานขององค์การ หน่วยงาน
+                    และพนักงานแต่ละคน และพัฒนาขีดความสามารถของพนักงานในทุกระดับ
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 portfolio-item">
+              <div className="card h-100">
+                <a href="#">
+                  <img className="card-img-top" src="images/p5.jpg" alt="" />
+                </a>
+                <div className="card-body">
+                  <h4 className="card-title">
+                    <a
+                      href="assets/Organization Intercvenhtion/Organization Intercvenhtion 42-75.pdf"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      การปรับโครงสร้างองค์การ{" "}
+                    </a>
+                  </h4>
+                  <p className="card-text">
+                    การปรับโครงสร้างองค์กรหมายถึงกระบวนการปรับโครงสร้างลำดับชั้น
+                    โครงสร้างภายใน หรือขั้นตอนการดำเนินงานของบริษัท บริษัทต่างๆ
+                    ดำเนินการปรับโครงสร้างเพื่อบรรลุเป้าหมายบางประการ เช่น
+                    เพื่อให้สามารถแข่งขันได้มากขึ้นหรือตอบสนองต่อการเปลี่ยนแปลง
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 portfolio-item">
+              <div className="card h-100">
+                <a href="#">
+                  <img className="card-img-top" src="images/p6.jpg" alt="" />
+                </a>
+                <div className="card-body">
+                  <h4 className="card-title">
+                    <a
+                      href="assets/Organization Intercvenhtion/Organization Intercvenhtion 76-111.pdf"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      การมีส่วนร่วมของพนักงาน
+                    </a>
+                  </h4>
+                  <p className="card-text">
+                    การมีส่วนร่วมของพนักงานคือการได้รับรวมเป็นส่วนหนึ่งของทีมอย่างเต็มตัว
+                    มุ่งเน้นไปที่เป้าหมายที่ชัดเจน ได้รับความไว้วางใจและอำนาจ
+                    ได้รับการตอบรับเชิงบวกและสร้างสรรค์อย่างสม่ำเสมอ
+                    ได้รับการสนับสนุนในการพัฒนาทักษะใหม่ๆ
+                    ได้รับการขอบคุณและได้รับการยอมรับสำหรับความสำเร็จ
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 portfolio-item">
+              <div className="card h-100">
+                <a href="#">
+                  <img className="card-img-top" src="images/p7.jpg" alt="" />
+                </a>
+                <div className="card-body">
+                  <h4 className="card-title">
+                    <a
+                      href="assets/Organization Intercvenhtion/Organization Intercvenhtion 112-142.pdf"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      กลยุทธ์การแข่งขันและการร่วมมือ
+                    </a>
+                  </h4>
+                  <p className="card-text">
+                    เป้าหมายของกลยุทธ์การแข่งขันแบบร่วมมือกันคือการทำให้คู่แข่งหลุดพ้นจากเกมผลรวมเป็นศูนย์
+                    ซึ่งผู้ชนะจะได้ทุกอย่างและผู้แพ้ต้องอยู่เฉยๆ โมเดล Value Net
+                    แทนที่แนวทางนั้นด้วยเกมผลรวมบวก ในโมเดลนี้
+                    ผลลัพธ์จะสร้างกำไรให้กับคู่แข่งทั้งหมดเมื่อพวกเขาทำงานร่วมกัน
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 portfolio-item">
+              <div className="card h-100">
+                <a href="#">
+                  <img className="card-img-top" src="images/p8.jpg" alt="" />
+                </a>
+                <div className="card-body">
+                  <h4 className="card-title">
+                    <a
+                      href="assets/Technostructural Interventional/Technostructural Interventional-1-32.pdf"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      การออกแบบงาน{" "}
+                    </a>
+                  </h4>
+                  <p className="card-text">
+                    การออกแบบงานเป็นวิธีการจัด ตำแหน่งหน้าที่
+                    ให้สอดคล้องกับความต้องการของบริษัท
+                    พร้อมทั้งให้แน่ใจว่าหน้าที่นั้นตอบสนองความต้องการของพนักงาน
+                    แทนที่จะลอกเลียนตำแหน่งงานขององค์กรอื่น
+                    หรือสร้างหรือออกแบบใหม่ตามความคุ้นเคย
+                    คุณควรจัดสรรบทบาทและความรับผิดชอบโดยคำนึงถึงการเพิ่มผลผลิตสูงสุดโดยทำให้ตำแหน่งนั้นมีประโยชน์ต่อพนักงาน
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 portfolio-item">
+              <div className="card h-100">
+                <a href="#">
+                  <img className="card-img-top" src="images/p9.jpg" alt="" />
+                </a>
+                <div className="card-body">
+                  <h4 className="card-title">
+                    <a
+                      href="assets/Technostructural Interventional/Technostructural Interventional-33-54.pdf"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      การ coaching
+                    </a>
+                  </h4>
+                  <p className="card-text">
+                    การโค้ช (Coaching)"
+                    คือกระบวนการทำงานร่วมกันระหว่างโค้ชซึ่งเป็นเพื่อนชวนคิด
+                    หรือปลดล๊อคบางอย่างในตัวผู้รับการโค้ช (Coachee อ่านว่า
+                    โค้ชชี่) มีศักยภาพสูงขึ้น หรือมีความสุขอย่างที่เขาต้องการ
+                    ผ่านวิธีการและเครื่องมือต่างๆ เพื่อให้ผู้รับการโค้ช
+                    ได้เรียนรู้ ตระหนักในตัวเองและเปลี่ยนแปลง
+                    และลงมือทำด้วยความคิด ความถนัด
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 portfolio-item">
+              <div className="card h-100">
+                <a href="#">
+                  <img className="card-img-top" src="images/p10.jpg" alt="" />
+                </a>
+                <div className="card-body">
+                  <h4 className="card-title">
+                    <a
+                      href="assets/Technostructural Interventional/Technostructural Interventional-55-77.pdf"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      การสร้างทีม
+                    </a>
+                  </h4>
+                  <p className="card-text">
+                    การสร้างทีมงาน หมายถึง
+                    ความพยายามที่จะเข้าใจพฤติกรรมของคนที่ต้องมาปฏิบัติงานร่วมกัน
+                    และทำให้กลุ่มคนเหล่านั้นสามารถเรียนรู้ วิเคราะห์ปัญหา
+                    และหาทางออกในการปฏิบัติงานร่วมกัน
+                    เพื่อให้งานบรรลุเป้าหมายที่วางไว้ ดังนั้น
+                    หน่วยงานหรือองค์กรที่จะประสบความสำเร็จ
+                    ต้องมีการสร้างทีมที่มีความสามัคคี และมีเป้าหมาย
                   </p>
                 </div>
               </div>
@@ -52,105 +209,24 @@ export default function Portfolio2() {
                 <a href="#">
                   <img
                     className="card-img-top"
-                    src="images/portfolio-big-02.jpg"
+                    src="images/p11.jpg"
                     alt=""
                   />
                 </a>
                 <div className="card-body">
                   <h4 className="card-title">
-                    <a href="#">Morbi eu nulla ac mauris euismod aliquet.</a>
+                    <a
+                      href="assets/Technostructural Interventional/Technostructural Interventional-78-123.pdf"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      การเปลี่ยนวัฒนธรรมองค์การ
+                    </a>
                   </h4>
                   <p className="card-text">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Fugit aliquam aperiam nulla perferendis dolor nobis numquam,
-                    rem expedita, aliquid optio, alias illum eaque. Non magni,
-                    voluptates quae, necessitatibus unde temporibus.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="col-lg-6 portfolio-item">
-              <div className="card h-100">
-                <a href="#">
-                  <img
-                    className="card-img-top"
-                    src="images/portfolio-big-03.jpg"
-                    alt=""
-                  />
-                </a>
-                <div className="card-body">
-                  <h4 className="card-title">
-                    <a href="#">Morbi eu nulla ac mauris euismod aliquet.</a>
-                  </h4>
-                  <p className="card-text">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
-                    viverra euismod odio, gravida pellentesque urna varius
-                    vitae.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="col-lg-6 portfolio-item">
-              <div className="card h-100">
-                <a href="#">
-                  <img
-                    className="card-img-top"
-                    src="images/portfolio-big-04.jpg"
-                    alt=""
-                  />
-                </a>
-                <div className="card-body">
-                  <h4 className="card-title">
-                    <a href="#">Morbi eu nulla ac mauris euismod aliquet.</a>
-                  </h4>
-                  <p className="card-text">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Fugit aliquam aperiam nulla perferendis dolor nobis numquam,
-                    rem expedita, aliquid optio, alias illum eaque. Non magni,
-                    voluptates quae, necessitatibus unde temporibus.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="col-lg-6 portfolio-item">
-              <div className="card h-100">
-                <a href="#">
-                  <img
-                    className="card-img-top"
-                    src="images/portfolio-big-01.jpg"
-                    alt=""
-                  />
-                </a>
-                <div className="card-body">
-                  <h4 className="card-title">
-                    <a href="#">Morbi eu nulla ac mauris euismod aliquet.</a>
-                  </h4>
-                  <p className="card-text">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
-                    viverra euismod odio, gravida pellentesque urna varius
-                    vitae.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="col-lg-6 portfolio-item">
-              <div className="card h-100">
-                <a href="#">
-                  <img
-                    className="card-img-top"
-                    src="images/portfolio-big-02.jpg"
-                    alt=""
-                  />
-                </a>
-                <div className="card-body">
-                  <h4 className="card-title">
-                    <a href="#">Morbi eu nulla ac mauris euismod aliquet.</a>
-                  </h4>
-                  <p className="card-text">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Fugit aliquam aperiam nulla perferendis dolor nobis numquam,
-                    rem expedita, aliquid optio, alias illum eaque. Non magni,
-                    voluptates quae, necessitatibus unde temporibus.
+                    การเปลี่ยนแปลงวัฒนธรรมองค์กรคือกระบวนการพัฒนาวัฒนธรรมองค์กรของคุณในรูปแบบที่แท้จริงซึ่งดึงดูดผู้คนและสร้างแรงบันดาลใจให้ทีมทำงานได้อย่างเต็มประสิทธิภาพ
+                    เป็นเรื่องของการกำหนดและปลูกฝังพฤติกรรมที่ต้องการและปรับปรุงประสบการณ์ของพนักงานเพื่อเพิ่มการมีส่วนร่วม
+                    การรักษาพนักงาน ขวัญกำลังใจ และช่วยให้องค์กรบรรลุเป้าหมาย
                   </p>
                 </div>
               </div>
@@ -192,6 +268,6 @@ export default function Portfolio2() {
         </div>
         {/* /.container */}
       </>
-      </div>
-  )
+    </div>
+  );
 }
